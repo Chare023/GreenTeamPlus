3e8d88fa7b83e507b2eeea327330aa78f4d09551
dragance023 <dragance023@outlook.com>
2021-09-25 17:40:30 +0200

    Footer WIP

--- footer.php
+++ footer.php
@@ -1,4 +1,42 @@
 <?php wp_footer();?>
-
+<footer class="l-footer">
+    <div class="l-footer-container">
+        <div class="l-footer__section l-footer__section--main">
+            
+        </div>
+        <div class="l-footer__section">
+            <p class="c-footer-title">Usluge</p>
+            <ul class="c-footer-list">
+                <li>Gipsarski radovi</li>
+                <li>Molerski i fasaderski radovi</li>
+                <li>Elektroinstalaterske usluge</li>
+                <li>Keramičarske usluge</li>
+                <li>Parketarske usluge</li>
+                <li>Zamena stolarije</li>
+                <li>Rušenje i odvoz šuta</li>
+            </ul>
+        </div>
+        <div class="l-footer__section">
+            <p class="c-footer-title">Kontakt</p>
+            <a href="" class="c-footer-link">
+                <span class="c-footer-link__icon"><i class="fas fa-phone-alt"></i></span>
+                <span>0651231233</span>
+            </a>
+            <a href="" class="c-footer-link">
+                <span class="c-footer-link__icon"></span>
+                <span>greenteamplus@gmail.com</span>
+            </a>
+            <a href="" class="c-footer-link">
+                <span class="c-footer-link__icon"></span>
+                <span>@greenteamplus</span>
+            </a>
+            <a href="" class="c-footer-link">
+                <span class="c-footer-link__icon"></span>
+                <span>greenteamplus</span>
+            </a>
+        </div>
+    </div>
+    <p>Copyright © 2021 greenteamplus.rs - sva prava zadržana.</p>
+</footer>
 </body>
 </html>
--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <?php wp_head(); ?>
+    <script src="https://kit.fontawesome.com/23930a8e62.js" crossorigin="anonymous"></script>
   </head>
   <body>
     <h1></h1>
